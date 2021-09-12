8ad587587256409b92a18e20d4cde11cc26e1e79
Ilia <smorodin.it@gmail.com>
2021-09-12 23:51:03 +0300

    fix

--- src/theme/cssSnippets.ts
+++ src/theme/cssSnippets.ts
@@ -4,6 +4,6 @@ export const w100 = css`
   width: 100%;
 `;
 
-export const checkWide = css<{ wide: boolean }>`
+export const checkWide = css<{ wide?: boolean }>`
   ${({ wide }) => (wide ? w100 : null)};
 `;
