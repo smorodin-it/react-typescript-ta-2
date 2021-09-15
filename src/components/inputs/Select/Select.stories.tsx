86e856301a67b7fadea8c4df7d7b8cc2a8b0533a
Ilia <smorodin.it@gmail.com>
2021-09-15 16:00:21 +0300

    FIX

--- src/components/inputs/Select/Select.stories.tsx
+++ src/components/inputs/Select/Select.stories.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { Select } from "./Select";
-import { OptionObject } from "./types";
 
 export default {
   title: "Component/Input",
