a5ad82c251364b6a2c7d6fa0462323a9c738570a
Ilia <smorodin.it@gmail.com>
2021-09-15 14:12:02 +0300

    Make base style for inputs

---
+++ src/components/inputs/BaseInputStyled/BaseInputStyled.ts
@@ -0,0 +1,28 @@
+import { css } from "styled-components/macro";
+
+export const BaseInputStyled = css`
+  margin-bottom: 8px;
+  padding: 16px 57px 15px 16px;
+
+  background: ${({ theme }) => theme.colors.white};
+
+  font-family: "IBM Plex Sans", sans-serif;
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.dark};
+
+  border: 1px solid ${({ theme }) => theme.colors.light};
+  border-radius: 6px;
+  box-shadow: 0px 4px 8px rgba(44, 39, 56, 0.04);
+
+  ::placeholder {
+    color: ${({ theme }) => theme.colors.glacier};
+  }
+
+  :active,
+  :focus {
+    margin: -1px -1px 7px;
+
+    border: 2px solid ${({ theme }) => theme.colors.blue};
+    outline: unset;
+  }
+`;
--- src/components/inputs/Input/styled/InputStyled.ts
+++ src/components/inputs/Input/styled/InputStyled.ts
@@ -1,31 +1,9 @@
 import styled from "styled-components/macro";
 import { InputProps } from "../types";
 import { checkWide } from "../../../../theme";
+import { BaseInputStyled } from "../../BaseInputStyled/BaseInputStyled";
 
 export const InputStyled = styled.input<InputProps>`
   ${checkWide};
-  margin-bottom: 8px;
-  padding: 16px 57px 15px 16px;
-
-  background: ${({ theme }) => theme.colors.white};
-
-  font-family: "IBM Plex Sans", sans-serif;
-  font-size: 16px;
-  color: ${({ theme }) => theme.colors.dark};
-
-  border: 1px solid ${({ theme }) => theme.colors.light};
-  border-radius: 6px;
-  box-shadow: 0px 4px 8px rgba(44, 39, 56, 0.04);
-
-  ::placeholder {
-    color: ${({ theme }) => theme.colors.glacier};
-  }
-
-  :active,
-  :focus {
-    margin: -1px -1px 7px;
-
-    border: 2px solid ${({ theme }) => theme.colors.blue};
-    outline: unset;
-  }
+  ${BaseInputStyled}
 `;
