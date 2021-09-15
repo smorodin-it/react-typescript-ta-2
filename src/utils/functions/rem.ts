02f41c2c55f5448048d389369abefad15689af4e
Ilia <smorodin.it@gmail.com>
2021-09-15 16:20:12 +0300

    FIX

--- src/utils/functions/rem.ts
+++ src/utils/functions/rem.ts
@@ -6,16 +6,16 @@
 export const rem = (data: string): string => {
   // Split all values
   const dataArray = data.split(" ");
-  const convertedArray = [];
+  const convertedArray = [] as string[];
   dataArray.forEach((element) => {
     // parse pixel value
     const pixels = parseInt(element);
     // Convert to rem. Important: change divider to html tag font size
-    let rem;
+    let rem: string;
     if (pixels !== 0) {
       rem = pixels / 16 + "rem";
     } else {
-      rem = 0;
+      rem = "0";
     }
     convertedArray.push(rem);
   });
