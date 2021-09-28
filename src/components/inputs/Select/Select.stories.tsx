349f516946fcd7d59ccc8e2f564c3715d9083d33
Ilia <smorodin.it@gmail.com>
2021-09-28 13:42:25 +0300

    select component stories

--- src/components/inputs/Select/Select.stories.tsx
+++ src/components/inputs/Select/Select.stories.tsx
@@ -15,12 +15,32 @@ export const BaseSelect = Template.bind({});
 BaseSelect.args = {
   label: "Label",
   placeholder: "Placeholder",
-  wide: false,
-  wideOptions: false,
   options: [
     {
-      label:
-        "Item 1Item 1Item 1Item 1Item 1Item 1Item 1Item 1Item 1Item 1Item 1Item 1",
+      label: "Item 1",
+      value: "item 1",
+    },
+    { label: "Item 2", value: "item 2" },
+    { label: "Item 3", value: "item 3" },
+    { label: "Item 4", value: "item 4" },
+    { label: "Item 5", value: "item 5" },
+    { label: "Item 6", value: "item 6" },
+  ],
+};
+
+export const WideSelect = Template.bind({});
+WideSelect.args = {
+  ...BaseSelect.args,
+  wide: true,
+};
+
+export const WideOptionsSelect = Template.bind({});
+WideOptionsSelect.args = {
+  ...BaseSelect.args,
+  wideOptions: true,
+  options: [
+    {
+      label: "Very long item option, that can go outside of the block",
       value: "item 1",
     },
     { label: "Item 2", value: "item 2" },
